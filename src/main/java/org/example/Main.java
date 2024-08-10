be4ade8a203fc73114d7d51b391ff1233644692e
Parsa Esfahani <parsaesfahani78@gmail.com>
2024-08-10 20:53:32 +0330

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        System.out.println("Hello world!");
+
+    }
+
+    private void test() {
+        System.out.println("bye!");
+    }
+
+}
